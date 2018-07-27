a27cab95de644690b558c73208dcda226c680b7c
Vitaliy Karpenko <v.karpenko@minexsystems.com>
2018-07-27 18:32:33 +0300

    Blocks info verification: links Test for paginator.

--- bitcoin/__init__.py
+++ bitcoin/__init__.py
@@ -19,33 +19,32 @@ import bitcoin.core
 __version__ = '0.10.2dev'
 
 class MainParams(bitcoin.core.CoreMainParams):
-    MESSAGE_START = b'\xf9\xbe\xb4\xd9'
-    DEFAULT_PORT = 8333
+    MESSAGE_START = b'\x4b\x4a\x4c\x5d'
+    DEFAULT_PORT = 8335
     RPC_PORT = 8332
-    DNS_SEEDS = (('bitcoin.sipa.be', 'seed.bitcoin.sipa.be'),
-                 ('bluematt.me', 'dnsseed.bluematt.me'),
-                 ('dashjr.org', 'dnsseed.bitcoin.dashjr.org'),
-                 ('bitcoinstats.com', 'seed.bitcoinstats.com'),
-                 ('xf2.org', 'bitseed.xf2.org'),
-                 ('bitcoin.jonasschnelli.ch', 'seed.bitcoin.jonasschnelli.ch'))
-    BASE58_PREFIXES = {'PUBKEY_ADDR':0,
+    DNS_SEEDS = (('138.197.73.48', '138.197.73.48'),
+                 ('138.197.73.123', '138.197.73.123'),
+                 ('159.203.70.193', '159.203.70.193'),
+                 ('88.198.33.35', '88.198.33.35'),
+                 ('95.85.35.152', '95.85.35.152'),
+                 ('78.46.93.126', '78.46.93.126'),
+                 ('91.233.111.28', '91.233.111.28'))
+    BASE58_PREFIXES = {'PUBKEY_ADDR':75,
                        'SCRIPT_ADDR':5,
                        'SECRET_KEY' :128}
 
 class TestNetParams(bitcoin.core.CoreTestNetParams):
-    MESSAGE_START = b'\x0b\x11\x09\x07'
-    DEFAULT_PORT = 18333
+    MESSAGE_START = b'\x54\x4a\x4c\x54'
+    DEFAULT_PORT = 8336
     RPC_PORT = 18332
-    DNS_SEEDS = (('testnetbitcoin.jonasschnelli.ch', 'testnet-seed.bitcoin.jonasschnelli.ch'),
-                 ('petertodd.org', 'seed.tbtc.petertodd.org'),
-                 ('bluematt.me', 'testnet-seed.bluematt.me'),
-                 ('bitcoin.schildbach.de', 'testnet-seed.bitcoin.schildbach.de'))
+    DNS_SEEDS = (('138.197.90.246', '138.197.90.246'),
+                 ('139.59.154.41', '139.59.154.41'))
     BASE58_PREFIXES = {'PUBKEY_ADDR':111,
                        'SCRIPT_ADDR':196,
                        'SECRET_KEY' :239}
 
 class RegTestParams(bitcoin.core.CoreRegTestParams):
-    MESSAGE_START = b'\xfa\xbf\xb5\xda'
+    MESSAGE_START = b'\x4b\x4a\x4c\x5d'
     DEFAULT_PORT = 18444
     RPC_PORT = 18443
     DNS_SEEDS = ()
